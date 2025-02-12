7f90e12063301ffac8f0dac6589b04b9008d0685
dshaw0004 <dipankarshaw692@gmail.com>
2025-02-12 23:58:12 +0530

    feat: about page is created

---
+++ app/about.tsx
@@ -0,0 +1,12 @@
+import {View} from 'react-native';
+import {Text} from 'react-native-paper';
+
+export default function About(){
+        return (
+                <View>
+                <Text>
+                this app does not own any of post data, it is just a wrapper around the dev.to api
+                </Text>
+                </View>
+        );
+}
